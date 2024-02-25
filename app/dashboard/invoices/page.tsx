c2e739514e3cd55953b66f4c983cd735addb879e
James Douglas <james@phytochemconsulting.ca>
2024-02-25 12:58:24 -0800

    layout must be at the dashboard level to keep the navbar intact

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -2,25 +2,6 @@ import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
-<<<<<<< HEAD
-import { lusitana } from '@/app/ui/fonts';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
-import { fetchInvoicesPages } from '@/app/lib/data'
- 
-export default async function Page({ 
-  searchParams, 
-}: { 
-  searchParams?:{
-    query?: string;
-    page?: string;
-  };
-}) {
-  const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;
-
-  const totalPages = await fetchInvoicesPages(query)
-=======
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { lusitana } from '@/app/ui/fonts';
@@ -38,7 +19,6 @@ export default async function Page({
   const currentPage = Number(searchParams?.page) || 1;
   
   const totalPages = await fetchInvoicesPages(query);
->>>>>>> try2
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -50,11 +30,7 @@ export default async function Page({
       </div>
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
-<<<<<<< HEAD
-      </Suspense>
-=======
       </Suspense> 
->>>>>>> try2
       <div className="mt-5 flex w-full justify-center">
         <Pagination totalPages={totalPages} />
       </div>
